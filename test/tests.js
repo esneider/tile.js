4075179c13536c306991199551835ba491717da2
esneider <dariosn@gmail.com>
2013-09-21 04:41:21 -0300

    More tests

--- test/tests.js
+++ test/tests.js
@@ -35,14 +35,24 @@ test('URL constructor', function() {
 
     t = Tile.fromUrl('http://mt1.google.com/vt/lyrs=m@129&hl=en&x=213&y=230&z=10&s=Galileo');
     deepEqual(t, new Tile(213, 230, 10), 'Param pattern');
+    t = Tile.fromUrl('http://mt1.google.com/vt/lyrs=m@129&hl=en&y=230&z=10&x=213&s=Galileo');
+    deepEqual(t, new Tile(213, 230, 10), 'Param pattern (shuffle)');
+    f = function() { Tile.fromUrl('http://mt1.google.com/vt/lyrs=m@129&hl=en&x=230&z=10&x=213&s=Galileo'); };
+    throws(f, 'Invalid url (param pattern: has no z)');
 
     t = Tile.fromUrl('http://domain.com/myimage_11_543_234.png');
     deepEqual(t, new Tile(543, 234, 11), 'Generic pattern');
+    f = function() { Tile.fromUrl('http://domain.com/myimage_11_543-234.png'); };
+    throws(f, 'Invalid url (generic pattern: has different separators)');
 
-    f = function() { Tile.fromUrl('https://www.google.com/'); };
+    f = function() { Tile.fromUrl(''); };
+    throws(f, 'Invalid url (empty)');
+    f = function() { Tile.fromUrl('http://www.google.com/'); };
     throws(f, 'Invalid url (non-numeric)');
-
-    f = function() { Tile.fromUrl('https://www.google.com/search?aqs=chrome..69i57j69i60l3j69i65l2.699j0'); };
+    f = function() { Tile.fromUrl('http://www.google.com/search?aqs=chrome..69i57j69i60l3j69i65l2.699j0'); };
     throws(f, 'Invalid url (numeric)');
+
+    t = Tile.fromUrl('http://mw1.google.com/mw-planetary/lunar/lunarmaps_v1/clem_bw/9/321/121.jpg', 'tms');
+    deepEqual(t, new Tile(321, (1 << 9) - 122, 9), 'TMS type');
 });
 
